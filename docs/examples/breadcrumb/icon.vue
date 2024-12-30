bf3961b618acd7db47d8592d82183beeda9583c2
SJJ021160 <1061768211@qq.com>
2024-12-30 11:26:18 +0800

    Update icon.vue

--- docs/examples/breadcrumb/icon.vue
+++ docs/examples/breadcrumb/icon.vue
@@ -3,7 +3,6 @@
     <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
     <el-breadcrumb-item>promotion management</el-breadcrumb-item>
     <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
